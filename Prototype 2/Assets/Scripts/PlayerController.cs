59a09d4cb97676616d621cee9da9fbe92d5a896d
hsans13 <59098993+hsans13@users.noreply.github.com>
2020-02-06 15:32:32 -0800

    finished 2.1 and started 2,.2

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControlller : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
+    public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +17,23 @@ public class PlayerControlller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        horizontalInput = Input.GetAxis("Horizontal")
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //lauch projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
