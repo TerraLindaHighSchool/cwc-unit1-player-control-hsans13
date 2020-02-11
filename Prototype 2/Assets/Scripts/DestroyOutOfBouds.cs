d865e8c55b1ee0f21e1bb6427901ed054f2126a9
hsans13 <59098993+hsans13@users.noreply.github.com>
2020-02-11 15:14:24 -0800

    completed unit 2.4

--- Prototype 2/Assets/Scripts/DestroyOutOfBouds.cs
+++ Prototype 2/Assets/Scripts/DestroyOutOfBouds.cs
@@ -22,6 +22,7 @@ public class DestroyOutOfBouds : MonoBehaviour
         }
         else if (transform.position.z < lowerBound)
         {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
 
